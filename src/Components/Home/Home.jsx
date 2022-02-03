c69157e7d4c80e379574e7ee01b9a76e59e9e7d6
Aloïs Guitton <aloisguitton@MacBook-Pro-de-Alois.local>
2022-02-03 19:31:35 +0100

    update to react hooks

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,24 +1,21 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import Helmet from "react-helmet";
-import Typography from "@material-ui/core/Typography";
+import {Typography} from "@mui/material";
 
-export class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+export function Home(props) {
 
-    render() {
-        return <>
-            <Helmet>
-                <title>WeDrivIt - Location de véhicules anciens</title>
-                <meta name="description"
-                      content="Location de voitures de collection entre particuliers ou professionnels dans votre région.
-                      Partez pour un week-end vintage, votre mariage ou une occasion spéciale."/>
-            </Helmet>
-            <div className="row">
-                <Typography></Typography>
-            </div>
-        </>
-    }
+    useEffect(() => {
+
+    }, [])
+
+    return <>
+        <Helmet>
+            <title></title>
+            <meta name="description"
+                  content=""/>
+        </Helmet>
+        <div className="row">
+            <Typography></Typography>
+        </div>
+    </>
 }
